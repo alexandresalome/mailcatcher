9f21bafa8c50c467ec726ef4899fff219de7b0a6
Matt Parker <matt@lamplightdb.co.uk>
2014-01-31 14:50:10 +0000

    Be sure to exclude quotes in the regex for the boundary

--- Mime/Part.php
+++ Mime/Part.php
@@ -109,7 +109,7 @@ class Part
             return;
         }
 
-        if (!preg_match('#^multipart/(alternative|mixed);\s*boundary="?(.*)"?$#', $contentType, $vars)) {
+        if (!preg_match('#^multipart/(alternative|mixed);\s*boundary="?([^"]*)"?$#', $contentType, $vars)) {
             throw new \InvalidArgumentException(sprintf('Unable to parse multipart header: "%s".', $contentType));
         }
 
