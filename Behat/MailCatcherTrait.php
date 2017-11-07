b9c7ed98ee0e5d793421fdaf5697355d6a3bec11
Omar Jbara <omar.Jbara2@gmail.com>
2017-11-07 18:02:07 +0100

    - Added negations

--- Behat/MailCatcherTrait.php
+++ Behat/MailCatcherTrait.php
@@ -40,14 +40,21 @@ trait MailCatcherTrait
     /**
      * @return Message
      */
-    protected function findMail($type, $value)
+    protected function findMail($type, $value, $negation = false)
     {
         $criterias = array($type => $value);
 
         $message = $this->getMailCatcherClient()->searchOne($criterias);
 
         if (null === $message) {
+            // If no message was found but we wanted to NOT see a message, return a fake message to make the test pass
+            if($negation) return new Message($this->mailCatcherClient);
             throw new \InvalidArgumentException(sprintf('Unable to find a message with criterias "%s".', json_encode($criterias)));
+        } else {
+            if($negation) {
+                // If a message was found but we wanted to NOT see a message, throw an exception
+                throw new \InvalidArgumentException(sprintf('A message corresponding to your criterias was found : "%s".', json_encode($criterias)));
+            }
         }
 
         return $message;
